e9928cd24074657131cc96f533f0b25f9760f481
slackhideo <burajirugaijin@yahoo.co.jp>
2016-05-19 22:05:56 -0300

    Atualização do server_udp.c

--- t6/server_udp.c
+++ t6/server_udp.c
@@ -6,15 +6,16 @@
 #include <unistd.h>
 
 #define SERVER_PORT 31472
-#define MAX_PENDING 5
 #define MAX_LINE 256
 
 #define ever (;;)
 
 int main() {
     struct sockaddr_in sin;
+    struct sockaddr_in peer;
     char buf[MAX_LINE];
     int len;
+    unsigned int peerlen = sizeof(peer);
     int s;
 
     /* build address data structure */
@@ -35,14 +36,15 @@ int main() {
     }
 
     for ever {
-        while((len = recvfrom(s, buf, sizeof(buf), 0, NULL, NULL))) {
+        while((len = recvfrom(s, buf, sizeof(buf), 0,
+                        (struct sockaddr *)&peer, &peerlen))) {
             if(len < 0) {
                 perror("server-udp: recvfrom");
             }
             else {
-                fputs(buf, stdout);
+                fprintf(stdout, "%s\n", buf);
                 if(sendto(s, buf, strlen(buf)+1, 0,
-                                NULL, 0) < 0) {
+                                (struct sockaddr *)&peer, peerlen) < 0) {
                     perror("server-udp: sendto");
                 }
             }
