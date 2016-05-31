72c9dd83239f86910fc14989074a159aee56e289
slackhideo <burajirugaijin@yahoo.co.jp>
2016-05-31 10:29:44 -0300

    Questão 6

---
+++ t6/Bclient_udp.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#define SERVER_PORT 31472
+#define MAX_LINE 256
+
+int main(int argc, char *argv[]) {
+    struct hostent *hp;
+    struct sockaddr_in sin;
+    char *host;
+    char buf[MAX_LINE];
+    int s;
+
+    if(argc != 2) {
+        fprintf(stderr, "usage: %s <host>\n", argv[0]);
+        exit(1);
+    }
+    host = argv[1];
+
+    /* translate host name into peer's IP address */
+    hp = gethostbyname(host);
+    if(!hp) {
+        fprintf(stderr, "client-udp: unknown host: %s\n", host);
+        exit(1);
+    }
+
+    /* build address data structure */
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    memcpy(&sin.sin_addr, hp->h_addr, hp->h_length);
+    sin.sin_port = htons(SERVER_PORT);
+
+    /* setup active open */
+    if((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("client-udp: socket");
+        exit(1);
+    }
+
+    if(connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+        perror("client-udp: connect");
+        close(s);
+        exit(1);
+    }
+
+    while(fgets(buf, sizeof(buf), stdin)) {
+        buf[MAX_LINE-1] = '\0';
+        if(send(s, buf, strlen(buf)+1, 0) < 0) {
+            perror("client-udp: sendto");
+        }
+        if(recv(s, buf, sizeof(buf), 0) > 0) {
+            fprintf(stdout, "%s\n", buf);
+        }
+    }
+
+    close(s);
+
+    return EXIT_SUCCESS;
+}
